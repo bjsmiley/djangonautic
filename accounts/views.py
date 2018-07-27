9aebfae89c822e1cef4b684d06ebf3e210e80b5d
Bryan Smiley <bjsmiley@ucdavis.edu>
2018-07-26 20:59:18 -0700

    can now create users

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # log user in
+            return redirect('articles:list') # name of the url in the articles app
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
