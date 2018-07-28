2a4c46f81d1deda88e66d4f97a3f30dcba860a6b
Bryan Smiley <bjsmiley@ucdavis.edu>
2018-07-28 15:55:21 -0700

    can logout

--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponse
 # Create your views here.
+# import logging
+# logger = logging.getLogger(__name__)
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            # log user in
+            user = form.save()
+            login(request, user)
             return redirect('articles:list') # name of the url in the articles app
     else:
         form = UserCreationForm()
@@ -20,3 +24,28 @@ def signup_view(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            #log user in
+            user = form.get_user() # rtrieve the user
+            login(request, user)  # log the user in
+            return redirect('articles:list') # name of the url in the articles app
+
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        # logger.error("here!")
+        return redirect('articles:list')
+    # else:
+    #     return HttpResponse("DIDN'T WORK")
